428347847b72c74cc78a1626ed42943d481277d7
Ifra Saleem <ifrasaleemcsengineer12@gmail.com>
2023-01-17 06:58:40 -0800

    Add

--- src/StartPage/StartPage.js
+++ src/StartPage/StartPage.js
@@ -8,14 +8,9 @@ const StartPage = () => {
   return (
     <div className="start">
         <div className="block">
-        <img src={logo} alt="logo" 
-        style={{height: "210px",width:"230px",marginLeft:"40px",marginTop:"30px"}} >
-
-        </img>
+        <img src={logo} alt="logo" className="logo"></img>
         <div className="block-inner">
-        <h1 style=
-        {{marginBottom: "25px",marginLeft:"40px",marginTop:"10px",fontSize:"20px"}}>
-            Welcome to Gas Cylinder</h1>
+        <h1 className = "heading">Welcome to Gas Cylinder</h1>       
         <div className="button-login">
         <Link to = "/login" style={{color: "white"}}>
         <button
@@ -23,16 +18,16 @@ const StartPage = () => {
         marginTop: "5px",height: "50px",fontSize:"15px",marginRight: "10px",outlineColor: "orange"}}>         
             Login As Super Admin
             </button>
-            </Link>
-            <Link to = "/AdminLogin" style={{color: "white"}}>
+        </Link>
+        <Link to = "/AdminLogin" style={{color: "white"}}>
         <button style={{marginTop: "5px",height: "50px",fontSize:"15px",outlineColor: "orange"}}>
-       Login As Admin</button></Link>
+             Login As Admin
+             </button>
+       </Link>
         </div>
         </div>
-        </div>
-      
-            </div>
-                      
+        </div>     
+            </div>                      
        
   );
 };
